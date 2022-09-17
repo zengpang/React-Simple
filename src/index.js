38404f43ecd1111e03065d9abb18738d436d83e4
zengpang <2516629927@qq.com>
2022-09-17 17:21:24 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -33,8 +33,30 @@ function createElement(tag,attrs,...children)
 const React={
     createElement
 };
-
+function render(vdom,container)
+{
+   let node;
+   if(typeof vdom==='string')
+   {
+    node=document.createTextNode(vdom);
+   }
+   if(typeof vdom=== 'object')
+   {
+     node=document.createElement(vdom.tag);
+     vdom.children.forEach(childVdom =>render(childVdom,node));
+   }
+   container.appendChild(node);
+}
+const ReactDOM={
+    render(vdom,container)
+    {
+      container.innerHTML='';
+      render(vdom,container);
+    }
+};
 let div=<h1 className="hello">
-  
+  <span>hello</span>
+  <span>world</span>
 </h1>;
 console.log(div);
+ReactDOM.render(div,document.body);
